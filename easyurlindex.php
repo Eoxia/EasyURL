3cc641b4ffa44efac414d38978f8e1577048d521
Nicolas Domenech <nicolas@eoxia.com>
2023-11-03 09:51:31 +0100

    #36 [Dashboard] add: shortener dashboard

---
+++ easyurlindex.php
@@ -0,0 +1,33 @@
+<?php
+/* Copyright (C) 2021-2023 EVARISK <technique@evarisk.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    easyurlindex.php
+ * \ingroup easyurl
+ * \brief   Home page of easyurl top menu
+ */
+
+// Load EasyURL environment
+if (file_exists('easyurl.main.inc.php')) {
+    require_once __DIR__ . '/easyurl.main.inc.php';
+} elseif (file_exists('../easyurl.main.inc.php')) {
+    require_once __DIR__ . '/../easyurl.main.inc.php';
+} else {
+    die('Include of easyurl main fails');
+}
+
+require_once __DIR__ . '/../saturne/core/tpl/index/index_view.tpl.php';
